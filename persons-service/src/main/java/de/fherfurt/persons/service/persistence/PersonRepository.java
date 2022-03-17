785022ef07f7dbdf779f1290b0670666712f88a4
Tran Anh Hoang <tran.anh.hoang@fh-erfurt.de>
2022-03-17 21:41:09 +0100

    Update EditingPersonData and all dependencies

--- persons-service/src/main/java/de/fherfurt/persons/service/persistence/PersonRepository.java
+++ persons-service/src/main/java/de/fherfurt/persons/service/persistence/PersonRepository.java
@@ -4,21 +4,25 @@ package de.fherfurt.persons.service.persistence;
 import de.fherfurt.persons.service.core.Person;
 import java.util.ArrayList;
 import java.util.List;
-public class PersonRepository
-{
+public class PersonRepository {
+    private static final PersonRepository PersonStorage = new PersonRepository();
+
     private final List<Person> storage = new ArrayList<>();
 
     public void persist(Person person) {
         storage.add(person);
     }
 
-    public List<Person> getPersonList()
-    {
+    public List<Person> getPersonList() {
         return storage;
     }
 
-    public void printArrayList()
-    {
+
+    public void printArrayList() {
         System.out.println(storage);
+    }
+
+    public static PersonRepository getInstance(){
+        return PersonStorage;
     }
 }
