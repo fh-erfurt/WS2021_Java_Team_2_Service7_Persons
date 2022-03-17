49c0ff1fd7fdd9551782b3535ac474b4a777f2be
Tran Anh Hoang <tran.anh.hoang@fh-erfurt.de>
2022-03-17 13:14:03 +0100

    init with personclient

--- persons-client/src/main/java/de/fherfurt/persons/client/PersonsClient.java
+++ persons-client/src/main/java/de/fherfurt/persons/client/PersonsClient.java
@@ -4,19 +4,27 @@ import java.util.Optional;
 /**
  * @author Tran Anh Hoang
  * @version 1.0.0.0
+ * This Interface defines the provided functionality for Person Service.
+ * It should be used by other services.
  */
-public interface PersonsClient
-{
+public interface PersonsClient {
     /**
      *
      * This Methode "findPersonUsingIteratorByPersonID" will
      * find a Person from the ArrayList by
-     * the PersonID with an Iterator
+     * the PersonID with an Iterator.
      * @param PersonID - this Parameter is needed for the Searching.
-     * @return Person with all her/his values
+     * @return Optional (Person) with all her/his values
      *
      */
     Optional findPersonUsingIteratorBy(int PersonID);
-    byte[] findPersonAvatarBy(int PersonID);
 
+
+    /**
+     * This methode "findPersonAvatarBy" will find a Person Avatar from a HashMap
+     * through a PersonID - Key.
+     * @param PersonID
+     * @return byte Code which
+     */
+    byte[] findPersonAvatarBy(int PersonID);
 }
