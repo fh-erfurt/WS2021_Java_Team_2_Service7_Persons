9db92f8267d6d2149d813e190d299c455fcc8fa9
Tran Anh Hoang <tran.anh.hoang@fh-erfurt.de>
2022-08-07 02:13:13 +0200

    Adding Todos and Business Logic

---
+++ src/test/java/de/fherfurt/persons/service/model/AddressTest.java
@@ -0,0 +1,14 @@
+package de.fherfurt.persons.service.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddressTest {
+
+
+    //TODO: Milena Unittest schreiben
+    @Test
+    void checkStringValid() {
+    }
+}
