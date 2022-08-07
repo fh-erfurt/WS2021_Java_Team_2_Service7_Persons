35d54de945410778160f3089f262d6446d8f8faf
Tran Anh Hoang <tran.anh.hoang@fh-erfurt.de>
2022-08-07 14:13:12 +0200

    Added some comments for bug tracking

--- src/main/java/de/fherfurt/persons/service/util/DataProvider.java
+++ src/main/java/de/fherfurt/persons/service/util/DataProvider.java
@@ -13,15 +13,17 @@ import lombok.Getter;
 
 public class DataProvider {
 
-    public static ArrayList<Person> createTestData() {
-
+    public static ArrayList<Person> createTestData() throws IOException {
 
         final ArrayList <Person> personTestList = new ArrayList<>();
         final Logger LOGGER = Logger.getLogger( RepositoryFactory.class.getSimpleName() );
         LOGGER.info("Init Data Provider");
 
+        //out comment this code for the issue tracking and in comment the personAvatar01
         //PersonAvatar personAvatar01 = new PersonAvatar(new File("images\\Meme_Macron.png").getAbsoluteFile().toString());
 
+
+        //Random Byte Array which represent the converting byte array of a image
         byte [] testByteAsImage1 = {80, 61, 41 ,18 ,110 };
         byte [] testByteAsImage2 = {32, 45, 21 ,41 ,92 };
         byte [] testByteAsImage3 = {45, 42, 23 ,48 ,111 };
@@ -56,8 +58,6 @@ public class DataProvider {
 
 
         return personTestList;
-
-
 
     }
 
