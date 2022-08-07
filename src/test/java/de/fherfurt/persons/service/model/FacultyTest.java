c52dc606c2a518e6e9c3454b8282aafef55a7849
Yatamashi <81873469+Yatamashi@users.noreply.github.com>
2022-08-07 19:02:55 +0200

    Unittests Milena

--- src/test/java/de/fherfurt/persons/service/model/FacultyTest.java
+++ src/test/java/de/fherfurt/persons/service/model/FacultyTest.java
@@ -1,12 +1,28 @@
 package de.fherfurt.persons.service.model;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+
 
 class FacultyTest {
 
     @Test
     void testCheckFacultyName() {
+
+        //given
+        String testFaculty = "ASP";
+        Faculty testInstance = new Faculty("Architektur und Stadtplanung");
+        //when
+
+        //then
+        Assertions.assertThat(testInstance.checkFacultyName("Architektur und Stadtplanung"))
+                .isEqualTo(testFaculty)
+                .isNotNull()
+                .isNotEqualTo("Architektur und Stadtplanung");
+
+        Assertions.assertThat(testInstance.checkFacultyName("Architektur und Stadtplanung "))
+                .isNotEqualTo("ASP");
     }
 }
